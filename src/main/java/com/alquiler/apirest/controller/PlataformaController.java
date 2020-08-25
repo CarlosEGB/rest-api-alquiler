37f5025c8947a64a68b93c7a216cf153e68e46dd
Carlos Garcia <carlos_9655@hotmail.com>
2020-08-25 18:03:03 -0500

    Se desarrollo los metodos GETs de la tabla Plataforma

--- src/main/java/com/alquiler/apirest/controller/PlataformaController.java
+++ src/main/java/com/alquiler/apirest/controller/PlataformaController.java
@@ -3,10 +3,9 @@ package com.alquiler.apirest.controller;
 import com.alquiler.apirest.entity.Plataforma;
 import com.alquiler.apirest.service.PlataformaService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
@@ -18,5 +17,15 @@ public class PlataformaController {
     @PostMapping("/plataforma")
     public Plataforma postPlataforma(@RequestBody Plataforma plataforma) {
         return plataformaService.postPlataforma(plataforma);
+    }
+
+    @GetMapping("/plataforma")
+    public List<Plataforma> getAllPlataforma() {
+        return plataformaService.getAllPlataforma();
+    }
+
+    @GetMapping("/plataforma/{id}")
+    public Plataforma getPlataforma(@PathVariable(name = "id") int idPlataforma) {
+        return plataformaService.getPlataforma(idPlataforma);
     }
 }
--- src/main/java/com/alquiler/apirest/service/PlataformaService.java
+++ src/main/java/com/alquiler/apirest/service/PlataformaService.java
@@ -5,6 +5,8 @@ import com.alquiler.apirest.repository.IPlataforma;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class PlataformaService {
 
@@ -13,5 +15,13 @@ public class PlataformaService {
 
     public Plataforma postPlataforma(Plataforma plataforma) {
         return plataformaRepo.save(plataforma);
+    }
+
+    public List<Plataforma> getAllPlataforma() {
+        return plataformaRepo.findAll();
+    }
+
+    public Plataforma getPlataforma(int idPlataforma) {
+        return plataformaRepo.findById(idPlataforma).get();
     }
 }
