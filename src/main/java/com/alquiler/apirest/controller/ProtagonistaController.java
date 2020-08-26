216f74b8d9c108abe54fd0ca0365dbdf73431a33
Carlos Garcia <carlos_9655@hotmail.com>
2020-08-26 15:40:33 -0500

    Se desarrollo los metodos GETs de la tabla Protagonista

--- src/main/java/com/alquiler/apirest/controller/ProtagonistaController.java
+++ src/main/java/com/alquiler/apirest/controller/ProtagonistaController.java
@@ -3,10 +3,9 @@ package com.alquiler.apirest.controller;
 import com.alquiler.apirest.entity.Protagonista;
 import com.alquiler.apirest.service.ProtagonistaService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
@@ -18,5 +17,15 @@ public class ProtagonistaController {
     @PostMapping("/protagonista")
     private Protagonista postProtagonista(@RequestBody Protagonista protagonista) {
         return protagonistaService.postProtagonista(protagonista);
+    }
+
+    @GetMapping("/protagonista")
+    private List<Protagonista> getAllProtagonistas() {
+        return protagonistaService.getAllProtagonistas();
+    }
+
+    @GetMapping("/protagonista/{id}")
+    private Protagonista getProtagonista(@PathVariable(name = "id") int idProtagonista) {
+        return protagonistaService.getProtagonistas(idProtagonista);
     }
 }
--- src/main/java/com/alquiler/apirest/service/ProtagonistaService.java
+++ src/main/java/com/alquiler/apirest/service/ProtagonistaService.java
@@ -5,6 +5,8 @@ import com.alquiler.apirest.repository.IProtagonista;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class ProtagonistaService {
 
@@ -13,5 +15,13 @@ public class ProtagonistaService {
 
     public Protagonista postProtagonista(Protagonista protagonista) {
         return protagonistaRepo.save(protagonista);
+    }
+
+    public List<Protagonista> getAllProtagonistas() {
+        return protagonistaRepo.findAll();
+    }
+
+    public Protagonista getProtagonistas(int idProtagonista) {
+        return protagonistaRepo.findById(idProtagonista).get();
     }
 }
